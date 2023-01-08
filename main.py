b82b6de772d028f21150497bccc8a4d776b9a609
Michal Plichta <mplichta@gmail.com>
2023-01-08 16:24:04 +0100

    add display MicroPython logo

--- main.py
+++ main.py
@@ -228,6 +228,19 @@ def pico_serial():
     gc.collect()
 
 
+def logo(display):
+    display.fill(0)
+    display.fill_rect(0, 0, 32, 32, 1)
+    display.fill_rect(2, 2, 28, 28, 0)
+    display.vline(9, 8, 22, 1)
+    display.vline(16, 2, 22, 1)
+    display.vline(23, 8, 22, 1)
+    display.fill_rect(26, 24, 2, 4, 1)
+    display.text('MicroPython', 40, 0, 1)
+    display.text('SSD1306', 40, 12, 1)
+    display.text('OLED 128x64', 40, 24, 1)
+
+
 if __name__ == '__main__':
     # main()
     pico_serial()
