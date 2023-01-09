4f7f038c66a3aeff762192fb5eb5d497fb77d65d
Michal Plichta <mplichta@gmail.com>
2023-01-09 19:14:39 +0100

    add keyboard_icon function and minor edits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import sleep, sleep_ms
 from waveshare import LCD3inch5
 
+import framebuf
 import uselect
 from machine import Pin
 import gc
@@ -19,30 +20,14 @@ def main():
     for i in range(12):
         lcd.fill_rect(i * 30 + 60, 100, 30, 50, display_color)
         display_color = display_color << 1
+    logo(lcd)
     lcd.show_up()
     while True:
-        coord_x_y = lcd.touch_get()
+        coord_x_y = lcd.absolute_coord()
         if coord_x_y is not None:
-            x_point = int((coord_x_y[1] - 430) * 480 / 3270)
-            if x_point > 480:
-                x_point = 480
-            elif x_point < 0:
-                x_point = 0
-            y_point = 320 - int((coord_x_y[0] - 430) * 320 / 3270)
-            if y_point > 220:
-                lcd.fill(lcd.WHITE)
-                if x_point < 120:
-                    lcd.fill_rect(0, 60, 120, 100, lcd.RED)
-                    lcd.text("Button0", 20, 110, lcd.WHITE)
-                elif x_point < 240:
-                    lcd.fill_rect(120, 60, 120, 100, lcd.RED)
-                    lcd.text("Button1", 150, 110, lcd.WHITE)
-                elif x_point < 360:
-                    lcd.fill_rect(240, 60, 120, 100, lcd.RED)
-                    lcd.text("Button2", 270, 110, lcd.WHITE)
-                else:
-                    lcd.fill_rect(360, 60, 120, 100, lcd.RED)
-                    lcd.text("Button3", 400, 110, lcd.WHITE)
+            x_point = int((coord_x_y[0] - 430) * 480 / 3270)
+            y_point = int((coord_x_y[1] - 430) * 320 / 3270)
+            print('****', f'x: {x_point:>3} y: {y_point:>3}\n')
         else:
             lcd.fill(lcd.WHITE)
             lcd.text("Button0", 20, 110, lcd.BLACK)
@@ -228,13 +213,13 @@ def pico_serial():
     gc.collect()
 
 
-def logo(display):
+def logo(display: framebuf.FrameBuffer):
     """
     Show MircoPython logo at LCD.
 
     :param display: lcd instance
     """
-    display.fill(0)
+    # display.fill(0)
     display.fill_rect(0, 0, 32, 32, 1)
     display.fill_rect(2, 2, 28, 28, 0)
     display.vline(9, 8, 22, 1)
@@ -242,10 +227,58 @@ def logo(display):
     display.vline(23, 8, 22, 1)
     display.fill_rect(26, 24, 2, 4, 1)
     display.text('MicroPython', 40, 0, 1)
-    display.text('SSD1306', 40, 12, 1)
-    display.text('OLED 128x64', 40, 24, 1)
+    display.text('Waveshare', 40, 12, 1)
+    display.text('IPS 320x480', 40, 24, 1)
+
+
+def main2():
+    """Demo code form Waveshare."""
+    lcd = LCD3inch5()
+    lcd.backlight(20)
+    lcd.fill(lcd.WHITE)
+    keyboard_icon(lcd, 'SE')
+    lcd.show_down()
+    while True:
+        coord = lcd.get_touchpoint()
+        if coord is not None:
+            if coord.y < 32:
+                if 420 < coord.x < 480:
+                    lcd.fill_rect(lcd.width - 68, lcd.height - 32, 68, 32, lcd.RED)
+        else:
+            lcd.fill(lcd.WHITE)
+            keyboard_icon(lcd, 'SE')
+        lcd.show_down()
+        sleep(0.1)
+
+
+def keyboard_icon(lcd, pos='SE'):
+    """
+    Show keyboard icon.
+
+    :param lcd: LDC instance
+    :param pos: 'SE' or 'NW'
+    """
+    if pos == 'SE':
+        # keyboard icon down right
+        lcd.rect(lcd.width - 68, lcd.height - 32, 68, 32, 1)
+        lcd.rect(lcd.width - 66, lcd.height - 30, 64, 28, 1)
+        for i in range(6):
+            lcd.fill_rect(10 * i + lcd.width - 60, lcd.height - 10, 4, 4, 0)
+            lcd.fill_rect(10 * i + lcd.width - 60, lcd.height - 18, 4, 4, 0)
+            lcd.fill_rect(10 * i + lcd.width - 60, lcd.height - 26, 4, 4, 0)
+        lcd.fill_rect(lcd.width - 48, lcd.height - 10, 28, 4, 0)
+    elif pos == 'NW':
+        # keyboard icon upper left
+        lcd.rect(0, 0, 68, 32, 1)
+        lcd.rect(2, 2, 64, 28, 1)
+        for i in range(6):
+            lcd.fill_rect(10 * i + 6, 6, 4, 4, 0)
+            lcd.fill_rect(10 * i + 6, 14, 4, 4, 0)
+            lcd.fill_rect(10 * i + 6, 22, 4, 4, 0)
+        lcd.fill_rect(18, 22, 28, 4, 0)
 
 
 if __name__ == '__main__':
+    main2()
     # main()
-    pico_serial()
+    # pico_serial()
