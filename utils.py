b04cb75e2aa9f9911965eb1219b6b67f69deea6a
Michal Plichta <mplichta@gmail.com>
2023-01-10 21:56:42 +0100

    change split_rect() function

--- utils.py
+++ utils.py
@@ -24,27 +24,29 @@ class Position:
 
 Coord = namedtuple('Coord', ('x', 'y'))
 Rect = namedtuple('Rect', ('x', 'y', 'w', 'h', 'c'))
+RectLow = namedtuple('RectLow', ('x', 'y', 'w', 'h', 'c'))
+RectHigh = namedtuple('RectHigh', ('x', 'y', 'w', 'h', 'c'))
 
 
-def split_rect(rect: Rect, height=160):
+def split_rect(rect: Rect, split=160):
     """
-    Split rectangle between upper and lower part.
+    Split rectangle between high and low part.
 
-    :param rect:
-    :param height: default 160
-    :return: Tuple of Rect or None
+    :param rect: rectangle tuple as Rect
+    :param split: default 160
+    :return: List of RectLow, RectHigh or None
     """
-    y1_mod = rect.y % height
-    y1_div = rect.y // height
-    y2_mod = (rect.y + rect.h) % height
-    y2_div = (rect.y + rect.h) // height
+    y1_mod = rect.y % split
+    y1_div = rect.y // split
+    y2_mod = (rect.y + rect.h) % split
+    y2_div = (rect.y + rect.h) // split
     lower, upper = None, None
 
     if not y1_div and not y2_div:
-        upper = rect
+        upper = RectHigh(*rect)
     elif y1_div and y2_div:
-        lower = Rect(rect.x, y1_mod, rect.w, rect.h, rect.c)
+        lower = RectLow(rect.x, y1_mod, rect.w, rect.h, rect.c)
     elif not y1_div and y2_div:
-        upper = Rect(rect.x, rect.y, rect.w, rect.h - y2_mod, rect.c)
-        lower = Rect(rect.x, 0, rect.w, y2_mod, rect.c)
-    return upper, lower
+        upper = RectHigh(rect.x, rect.y, rect.w, rect.h - y2_mod, rect.c)
+        lower = RectLow(rect.x, 0, rect.w, y2_mod, rect.c)
+    return [upper, lower]
