e21f56f438781427fbbc32a38823461845f4b5cd
Michal Plichta <mplichta@gmail.com>
2023-01-10 22:14:49 +0100

    remove not needed code

--- main.py
+++ main.py
@@ -2,48 +2,8 @@ from time import sleep
 
 from framebuf import FrameBuffer
 
-from pico import Pico
 from utils import Color, Position, Rect, split_rect, RectHigh, RectLow
 from waveshare import LCD3inch5
-
-
-def main():
-    """Demo code form Waveshare."""
-    lcd = LCD3inch5()
-    lcd.backlight(20)
-    lcd.fill(Color.WHITE)
-    lcd.fill_rect(140, 5, 200, 30, Color.RED)
-    lcd.text("Raspberry Pi Pico", 170, 17, Color.WHITE)
-    display_color = 0x001F
-    lcd.text("3.5' IPS LCD TEST", 170, 57, Color.BLACK)
-    for i in range(12):
-        lcd.fill_rect(i * 30 + 60, 100, 30, 50, display_color)
-        display_color = display_color << 1
-    logo(lcd)
-    lcd.show_up()
-    while True:
-        coord_x_y = lcd.absolute_coord()
-        if coord_x_y is not None:
-            x_point = int((coord_x_y[0] - 430) * 480 / 3270)
-            y_point = int((coord_x_y[1] - 430) * 320 / 3270)
-            print('****', f'x: {x_point:>3} y: {y_point:>3}\n')
-        else:
-            lcd.fill(Color.WHITE)
-            lcd.text("Button0", 20, 110, Color.BLACK)
-            lcd.text("Button1", 150, 110, Color.BLACK)
-            lcd.text("Button2", 270, 110, Color.BLACK)
-            lcd.text("Button3", 400, 110, Color.BLACK)
-
-        lcd.show_down()
-        sleep(0.1)
-
-
-def pico_serial():
-    """Run Pico example."""
-    pico = Pico()
-    pico.main()
-    # when the above exits, clean up
-    gc.collect()
 
 
 def logo(display: FrameBuffer):
