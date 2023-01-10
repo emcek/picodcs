8e4ca5e399801120a229053f171f0534f71010b7
Michal Plichta <mplichta@gmail.com>
2023-01-10 22:14:31 +0100

    use update display high and low

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import gc
 from time import sleep
 
 from framebuf import FrameBuffer
@@ -65,29 +64,46 @@ def logo(display: FrameBuffer):
     display.text('IPS 320x480', 40, 24, 1)
 
 
+def update_display(lcd: LCD3inch5, draw_list: list, bg=Color.WHITE):
+    """
+    Update LCD, both high and low.
+
+    :param lcd: LCD instance
+    :param draw_list: list or drawings
+    :param bg: background color, default white
+    """
+    lcd.fill(bg)
+    for up in [upper for upper in draw_list if isinstance(upper, RectHigh)]:
+        lcd.fill_rect(*up)
+    lcd.show_up()
+
+    lcd.fill(bg)
+    for low in [lower for lower in draw_list if isinstance(lower, RectLow)]:
+        lcd.fill_rect(*low)
+    lcd.show_down()
+
+
 def show_keyboard():
     """Show clickable keyboard icon."""
     lcd = LCD3inch5()
     lcd.backlight(20)
-    lcd.fill(Color.WHITE)
+    key_draw = []
     while True:
         coord = lcd.get_touchpoint()
         if coord is not None:
             if coord.y < 32:
                 if 420 < coord.x < 480:
-                    keyboard_icon(lcd, Position.SE, True)
+                    key_draw = keyboard_icon(Position.SE, True)
         else:
-            lcd.fill(Color.WHITE)
-            keyboard_icon(lcd, Position.SE)
-        lcd.show_down()
+            key_draw = keyboard_icon(Position.SE)
+        update_display(lcd, key_draw)
         sleep(0.1)
 
 
-def keyboard_icon(lcd: LCD3inch5, pos=Position.SE, invert=False):
+def keyboard_icon(pos=Position.SE, invert=False):
     """
     Show keyboard icon.
 
-    :param lcd: LCD instance
     :param pos: 'SE', 'NW', 'NE', 'SW'
     :param invert: invert colors
     """
@@ -97,7 +113,7 @@ def keyboard_icon(lcd: LCD3inch5, pos=Position.SE, invert=False):
         foreground = Color.WHITE
         background = Color.BLACK
     ico_w, ico_h = 68, 32
-    dpl_w, dpl_h = 480, 160
+    dpl_w, dpl_h = 480, 320
     if pos == Position.SE:
         pos_x, pos_y = dpl_w, dpl_h
     elif pos == Position.NW:
@@ -109,14 +125,15 @@ def keyboard_icon(lcd: LCD3inch5, pos=Position.SE, invert=False):
     x = pos_x - ico_w
     y = pos_y - ico_h
 
-    upper, lower = [], []
-    upper1, lower1 = split_rect(Rect(x, y, ico_w, ico_h, foreground))
-    split_rect(Rect(x + 2, y + 2, ico_w - 4, ico_h - 4, background))
+    keyboard = split_rect(Rect(x, y, ico_w, ico_h, foreground))
+    keyboard.extend(split_rect(Rect(x, y, ico_w, ico_h, foreground)))
+    keyboard.extend(split_rect(Rect(x + 2, y + 2, ico_w - 4, ico_h - 4, background)))
     for i in range(6):
-        split_rect(Rect(10 * i + x + 6, y + 6, 4, 4, foreground))
-        split_rect(Rect(10 * i + x + 6, y + 14, 4, 4, foreground))
-        split_rect(Rect(10 * i + x + 6, y + 22, 4, 4, foreground))
-    split_rect(Rect(x + 18, y + 22, 28, 4, foreground))
+        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 6, 4, 4, foreground)))
+        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 14, 4, 4, foreground)))
+        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 22, 4, 4, foreground)))
+    keyboard.extend(split_rect(Rect(x + 18, y + 22, 28, 4, foreground)))
+    return keyboard
 
 
 def demo_split_rect():
@@ -140,7 +157,7 @@ def demo_split_rect():
 
 
 if __name__ == '__main__':
-    # show_keyboard()
-    demo_split_rect()
+    show_keyboard()
+    # demo_split_rect()
     # main()
     # pico_serial()
