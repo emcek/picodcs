08cebbc9eee4e6877436c66b987fc7453f3c760c
Michal Plichta <mplichta@gmail.com>
2023-01-10 23:35:23 +0100

    use old method to show icon and update display (faster)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from time import sleep
 
-from framebuf import FrameBuffer
-
 from utils import Color, Position, Rect, split_rect, RectHigh, RectLow
 from waveshare import LCD3inch5
 
 
-def logo(display: FrameBuffer):
+def logo(display: LCD3inch5):
     """
     Show MircoPython logo at LCD.
 
@@ -24,46 +22,30 @@ def logo(display: FrameBuffer):
     display.text('IPS 320x480', 40, 24, 1)
 
 
-def update_display(lcd: LCD3inch5, draw_list: list, bg=Color.WHITE):
-    """
-    Update LCD, both high and low.
-
-    :param lcd: LCD instance
-    :param draw_list: list or drawings
-    :param bg: background color, default white
-    """
-    lcd.fill(bg)
-    for up in [upper for upper in draw_list if isinstance(upper, RectHigh)]:
-        lcd.fill_rect(*up)
-    lcd.show_up()
-
-    lcd.fill(bg)
-    for low in [lower for lower in draw_list if isinstance(lower, RectLow)]:
-        lcd.fill_rect(*low)
-    lcd.show_down()
-
-
 def show_keyboard():
-    """Show clickable keyboard icon."""
+    """Demo code form Waveshare."""
     lcd = LCD3inch5()
     lcd.backlight(20)
-    key_draw = []
+    lcd.fill(Color.WHITE)
+    lcd.show_up()
     while True:
         coord = lcd.get_touchpoint()
         if coord is not None:
             if coord.y < 32:
                 if 420 < coord.x < 480:
-                    key_draw = keyboard_icon(Position.SE, True)
+                    keyboard_icon(lcd, Position.SE, True)
         else:
-            key_draw = keyboard_icon(Position.SE)
-        update_display(lcd, key_draw)
+            lcd.fill(Color.WHITE)
+            keyboard_icon(lcd, Position.SE)
+        lcd.show_down()
         sleep(0.1)
 
 
-def keyboard_icon(pos=Position.SE, invert=False):
+def keyboard_icon(lcd: LCD3inch5, pos=Position.SE, invert=False):
     """
     Show keyboard icon.
 
+    :param lcd: Display instance
     :param pos: 'SE', 'NW', 'NE', 'SW'
     :param invert: invert colors
     """
@@ -73,7 +55,7 @@ def keyboard_icon(pos=Position.SE, invert=False):
         foreground = Color.WHITE
         background = Color.BLACK
     ico_w, ico_h = 68, 32
-    dpl_w, dpl_h = 480, 320
+    dpl_w, dpl_h = 480, 160
     if pos == Position.SE:
         pos_x, pos_y = dpl_w, dpl_h
     elif pos == Position.NW:
@@ -85,15 +67,14 @@ def keyboard_icon(pos=Position.SE, invert=False):
     x = pos_x - ico_w
     y = pos_y - ico_h
 
-    keyboard = split_rect(Rect(x, y, ico_w, ico_h, foreground))
-    keyboard.extend(split_rect(Rect(x, y, ico_w, ico_h, foreground)))
-    keyboard.extend(split_rect(Rect(x + 2, y + 2, ico_w - 4, ico_h - 4, background)))
+    lcd.fill_rect(x, y, ico_w, ico_h, foreground)
+    lcd.fill_rect(x, y, ico_w, ico_h, foreground)
+    lcd.fill_rect(x + 2, y + 2, ico_w - 4, ico_h - 4, background)
     for i in range(6):
-        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 6, 4, 4, foreground)))
-        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 14, 4, 4, foreground)))
-        keyboard.extend(split_rect(Rect(10 * i + x + 6, y + 22, 4, 4, foreground)))
-    keyboard.extend(split_rect(Rect(x + 18, y + 22, 28, 4, foreground)))
-    return keyboard
+        lcd.fill_rect(10 * i + x + 6, y + 6, 4, 4, foreground)
+        lcd.fill_rect(10 * i + x + 6, y + 14, 4, 4, foreground)
+        lcd.fill_rect(10 * i + x + 6, y + 22, 4, 4, foreground)
+    lcd.fill_rect(x + 18, y + 22, 28, 4, foreground)
 
 
 def demo_split_rect():
@@ -119,5 +100,3 @@ def demo_split_rect():
 if __name__ == '__main__':
     show_keyboard()
     # demo_split_rect()
-    # main()
-    # pico_serial()
